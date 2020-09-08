1c7d5ee6b1aed662308265d61ec97f34b24de78f
Jeppevinkel <jeppevinkel@gmail.com>
2020-09-09 00:19:01 +0200

    Update assembly info

--- ExLocalization/Properties/AssemblyInfo.cs
+++ ExLocalization/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ExLocalization")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Centralized localization for EXILED plugins")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Jeppevinkel")]
 [assembly: AssemblyProduct("ExLocalization")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
